815a6c2eac9869a5ad1e8dc883634fde4f7c7df3
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2021-08-27 15:27:05 -0500

    trying php

--- src/server/index.js
+++ src/server/index.js
@@ -27,7 +27,7 @@ var con = mysql.createConnection({
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // const con = mysql.createConnection({
 //   host: "localhost",
